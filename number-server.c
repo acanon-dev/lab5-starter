262ae16f86709ab9567eddb6ee59de9ffdece751
acanon-dev <ancanon@ucsd.edu>
2024-11-01 03:44:15 +0000

    Commit

--- number-server.c
+++ number-server.c
@@ -1,38 +1,83 @@
 #include "http-server.h"
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-char const HTTP_404_NOT_FOUND[] = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n";
+int num = 0; // Global variable to hold the number
 
-void handle_404(int client_sock, char *path)  {
+// Function prototypes
+void handle_404(int client_sock, char *path);
+void handle_shownum(int client_sock);
+void handle_increment(int client_sock);
+void handle_add(int client_sock, const char *query);
+
+// Implement handle_404
+void handle_404(int client_sock, char *path) {
     printf("SERVER LOG: Got request for unrecognized path \"%s\"\n", path);
-
-    char response_buff[BUFFER_SIZE];
-    snprintf(response_buff, BUFFER_SIZE, "Error 404:\r\nUnrecognized path \"%s\"\r\n", path);
-    // snprintf includes a null-terminator
-
-    // TODO: send response back to client?
+    const char *header = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n";
+    char response_body[BUFFER_SIZE];
+    snprintf(response_body, BUFFER_SIZE, "Error 404: Unrecognized path \"%s\"\n", path);
+    
+    write(client_sock, header, strlen(header));
+    write(client_sock, response_body, strlen(response_body));
 }
 
+void handle_shownum(int client_sock) {
+    const char *header = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n";
+    char response_body[BUFFER_SIZE];
+    snprintf(response_body, BUFFER_SIZE, "Your number is %d\n", num);
+    
+    write(client_sock, header, strlen(header));
+    write(client_sock, response_body, strlen(response_body));
+}
+
+void handle_increment(int client_sock) {
+    num++; // Increment the global number
+    const char *header = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n";
+    char response_body[BUFFER_SIZE];
+    snprintf(response_body, BUFFER_SIZE, "Your number is now %d\n", num);
+    
+    write(client_sock, header, strlen(header));
+    write(client_sock, response_body, strlen(response_body));
+}
+
+void handle_add(int client_sock, const char *query) {
+    int value = 0;
+    sscanf(query, "value=%d", &value); // Extract the value from the query string
+    num += value; // Add the value to the global number
+    const char *header = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n";
+    char response_body[BUFFER_SIZE];
+    snprintf(response_body, BUFFER_SIZE, "Your number is now %d\n", num);
+    
+    write(client_sock, header, strlen(header));
+    write(client_sock, response_body, strlen(response_body));
+}
 
 void handle_response(char *request, int client_sock) {
     char path[256];
-
     printf("\nSERVER LOG: Got request: \"%s\"\n", request);
 
-    // Parse the path out of the request line (limit buffer size; sscanf null-terminates)
+    // Parse the path out of the request line
     if (sscanf(request, "GET %255s", path) != 1) {
         printf("Invalid request line\n");
         return;
     }
 
-    handle_404(client_sock, path);
+    if (strcmp(path, "/shownum") == 0) {
+        handle_shownum(client_sock);
+    } else if (strcmp(path, "/increment") == 0) {
+        handle_increment(client_sock);
+    } else if (strncmp(path, "/add?", 5) == 0) {
+        handle_add(client_sock, path + 5); // Pass the query string
+    } else {
+        handle_404(client_sock, path); // Handle unknown paths
+    }
 }
 
 int main(int argc, char *argv[]) {
     int port = 0;
-    if(argc >= 2) { // if called with a port number, use that
+    if (argc >= 2) { // If called with a port number, use that
         port = atoi(argv[1]);
     }
-
     start_server(&handle_response, port);
 }
